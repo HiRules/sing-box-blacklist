0a359be15c734bacd0907aa5a9b4e06293255811
HiRules <hitmp@outlook.com>
2024-08-29 22:50:13 +0800

    Update do.py

--- do.py
+++ do.py
@@ -2,6 +2,29 @@ import os
 import requests
 import json
 
+
+import subprocess
+
+def get_hidden_branch_file_path():
+    try:
+        # 切换到 hidden 分支
+        subprocess.run(['git', 'checkout', 'hidden'], check=True)
+        
+        # 获取 list.txt 文件的路径
+        file_path = 'blacklist.txt'
+        
+        return file_path
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        return None
+
+path = get_hidden_branch_file_path()
+if path:
+    print(path)
+else:
+    print("Failed to switch to hidden branch.")
+
+
 output_dir = "./release"
 
 blacklist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/blacklist.txt'
