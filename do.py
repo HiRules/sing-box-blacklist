d50f1020cfc8263debcafca70cfbd8769f080b10
HiRules <hitmp@outlook.com>
2024-08-30 21:38:42 +0800

    Update do.py

--- do.py
+++ do.py
@@ -56,7 +56,7 @@ def classify_content(new_list, url):
     for item in new_list:
         if item.startswith('.'):
             domain_suffix.append(item)
-        elif item.find('.'):
+        elif item.find('.') != -1:
             domain.append(item)
         else:
             domain_keyword.append(item)
