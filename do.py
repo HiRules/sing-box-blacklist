010c06244eb75b098e39c0133415f940ad1d6cee
HiRules <hitmp@outlook.com>
2024-09-02 12:56:54 +0800

    Update do.py

--- do.py
+++ do.py
@@ -5,11 +5,17 @@ import subprocess
 
 output_dir = "./release"
 
-blacklist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/blacklist.txt'
-excludelist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/excludelist.txt'
-blocklist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/blocklist.txt'
+subprocess.run(['git', 'checkout', 'hidden'], check=True)
+blacklist = 'blacklist.txt'
+excludelist = 'excludelist.txt'
+blocklist = 'blocklist.txt'
+custom_excludelist = 'custom_excludelist.txt'
 
-custom_excludelist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/custom_excludelist.txt'
+# blacklist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/blacklist.txt'
+# excludelist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/excludelist.txt'
+# blocklist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/blocklist.txt'
+
+# custom_excludelist = 'https://raw.githubusercontent.com/HiRules/sing-box-blacklist/hidden/custom_excludelist.txt'
 
 files = []
 
@@ -20,10 +26,14 @@ def pull_filename(url):
     return filename
 
 
-def read_urls_from_file(filepath):
-    respone = requests.get(filepath)
-    respone = respone.text.splitlines()
-    return respone
+def read_urls_from_file(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        print(lines[1])
+        return lines[1]
+    #respone = requests.get(filepath)
+    #respone = respone.text.splitlines()
+    #return respone
 
 
 def fetch_and_deduplicate_content(urls):
@@ -44,6 +54,17 @@ def fetch_and_deduplicate_content(urls):
     return content_set
 
 
+def read_and_process_excludelist(excludelist):
+    try:
+        with open(excludelist, 'r') as file:
+            lines = file.readlines()
+            print(lines[1])
+            return lines[1]
+    except FileNotFoundError:
+        print(f"File not found: {excludelist}")
+        return []
+
+
 def process_and_filter_content(content_list, domain_list):
     new_list = []
     for content in content_list:
@@ -52,7 +73,7 @@ def process_and_filter_content(content_list, domain_list):
     return new_list
 
 
-def classify_content(new_list, url):
+def classify_content(new_list, file):
     data = []
     domain = []
     domain_suffix = []
@@ -74,7 +95,7 @@ def classify_content(new_list, url):
         "version": 1,
         "rules": data
     }
-    filepath = os.path.join(output_dir, pull_filename(url) + ".json")
+    filepath = os.path.join(output_dir, file.split('.')[0] + ".json")
     with open(filepath, 'w') as f:
         f.write(json.dumps(result, indent=4))
         print(f"Successfully generated JSON file {filepath}.")
@@ -91,8 +112,9 @@ def convert_json_to_srs(json_file):
         print(f"Error converting JSON to SRS: {e}")
 
 def result(lists, ce):
-    ce = requests.get(ce)
-    ce = ce.text.splitlines()
+    #ce = requests.get(ce)
+    #ce = ce.text.splitlines()
+    read_and_process_excludelist(ce)
     
     for list in lists:
         e = read_urls_from_file(list)
@@ -101,21 +123,6 @@ def result(lists, ce):
         e = classify_content(e, list)
         e = convert_json_to_srs(e)
     return e
-
-
-def read_list_file():
-    file_path = 'custom_blacklist.txt'
-
-    try:
-        with open(file_path, 'r') as file:
-            content = file.readlines()
-            return content
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-        return []
-
-list_content = read_list_file()
-print(list_content)
 
 
 def main():
