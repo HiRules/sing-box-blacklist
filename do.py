1a409df6f37c063bae8b4ccf951ae1970396af29
HiRules <hitmp@outlook.com>
2024-09-09 17:46:34 +0800

    Update do.py

--- do.py
+++ do.py
@@ -5,14 +5,13 @@ import json
 import subprocess
 
 
-files = []
 output_dir = "release"
-blacklist = 'blacklist.txt'
-excludelist = 'excludelist.txt'
-blocklist = 'blocklist.txt'
+proxy_list = 'proxy_list.txt'
+exclude_proxy_list = 'exclude_proxy_list.txt'
+reject_list = 'reject_list.txt'
 geosite_cn = 'geosite_cn.txt'
 geoip_cn = 'geoip_cn.txt'
-custom_excludelist = 'custom_excludelist.txt'
+custom_exclude_list = 'custom_exclude_list.txt'
 
 
 def read_urls_from_file(file):
@@ -21,29 +20,63 @@ def read_urls_from_file(file):
         return lines
 
 
-def read_domain_from_excludelist(file):
-    try:
-        with open(file, 'r') as f:
-            lines = f.read().splitlines()
-            return lines
-    except FileNotFoundError:
-        print(f"File not found: {excludelist}")
-        return []
+def proxy_list(file, custom_exclude_list):
+    merged_file = set()
+    excluded_file = []
+    data = []
+    domain = []
+    domain_suffix = []
+    domain_keyword = []
+    with open(file, 'r') as f:
+        urls = f.read().splitlines()
+    for url in urls:
+        response = requests.get(url)
+        if response.status_code == 200:
+            lines = response.text.splitlines()
+            for line in lines:
+                e = line.strip()
+                if e:
+                    merged_file.add(e)
+    merged_file = list(merged_file)
+    merged_file.sort()
+    for line in merged_file:
+        if not any(line.startswith(domain) for domain in custom_exclude_list):
+            excluded_file.append(line)
+    for line in excluded_file:
+        if line.startswith('.'):
+            line.lstrip('.')
+            domain_suffix.append(line)
+        elif line.count('.') > 0:
+            domain.append(line)
+        else:
+            domain_keyword.append(line)
+    if domain:
+        data.append({"domain": domain})
+    if domain_suffix:
+        data.append({"domain_suffix": domain_suffix})
+    if domain_keyword:
+        data.append({"domain_keyword": domain_keyword})
+    result = {
+        "version": 1,
+        "rules": data
+    }
+    filepath = os.path.join(output_dir, file.split('.')[0] + ".json")
+    with open(filepath, 'w') as f:
+        f.write(json.dumps(result, indent=4))
+        print(f"Successfully generated JSON file {filepath}.")
+    return filepath
 
 
 def fetch_and_deduplicate_content(urls):
     result = set()
     for url in urls:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                lines = response.text.splitlines()
-                for line in lines:
-                    e = line.strip()
-                    if e:
-                        result.add(e)
-        except Exception as e:
-            print(f"Error fetching {url}: {e}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            lines = response.text.splitlines()
+            for line in lines:
+                e = line.strip()
+                if e:
+                    result.add(e)
     result = list(result)
     result.sort()
     return result
@@ -52,48 +85,29 @@ def fetch_and_deduplicate_content(urls):
 def fetch_and_deduplicate_cn_domain(urls):
     result = set()
     for url in urls:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                lines = response.text.splitlines()
-                for line in lines:
-                    if not line.startswith("#"):
-                        domain = re.match(r"server=\/(.*)\/(.*)", line)
-                        domain = domain.group(1)
-                        if domain.count('.') > 0:
-                            domain = '.' + domain
-                            result.add(domain)
-                        else:
-                            result.add(domain)
-        except Exception as e:
-            print(f"Error fetching {url}: {e}")
+        response = requests.get(url)
+        if response.status_code == 200:
+            lines = response.text.splitlines()
+            for line in lines:
+                if not line.startswith("#"):
+                    domain = re.match(r"server=\/(.*)\/(.*)", line)
+                    domain = domain.group(1)
+                    if domain:
+                        result.add(domain)
     result = list(result)
     result.sort()
     return result
 
 
-def process_and_filter_content(content_list, domain_list):
-    new_list = []
-    for content in content_list:
-        if not any(content.startswith(domain) for domain in domain_list):
-            new_list.append(content)
-    return new_list
-
-
 def json_of_domain(new_list, file):
     data = []
-    domain = []
     domain_suffix = []
     domain_keyword = []
     for item in new_list:
-        if item.startswith('.'):
-            domain_suffix.append(item)
         elif item.count('.') > 0:
-            domain.append(item)
+            domain_suffix.append(item)
         else:
             domain_keyword.append(item)
-    if domain:
-        data.append({"domain": domain})
     if domain_suffix:
         data.append({"domain_suffix": domain_suffix})
     if domain_keyword:
@@ -138,28 +152,40 @@ def convert_json_to_srs(json_file):
         print(f"Error converting JSON to SRS: {e}")
 
 
-def result_of_gfw_domain(lists, ce):
-    for list in lists:
-        e = read_urls_from_file(list)
-        e = fetch_and_deduplicate_content(e)
-        e = process_and_filter_content(e, read_domain_from_excludelist(ce))
-        e = json_of_domain(e, list)
-        e = convert_json_to_srs(e)
-    return e
-
-
-def result_of_cn_domain(list):
-    e = read_urls_from_file(list)
-    e = fetch_and_deduplicate_cn_domain(e)
-    e = json_of_domain(e, list)
+def result_of_gfw_domain(file, custom_exclude_list):
+    e = proxy_list(file, custom_exclude_list)
     e = convert_json_to_srs(e)
     return e
 
 
-def result_of_ip(list):
-    e = read_urls_from_file(list)
+def result_of_exclude_proxy_domain(file):
+    e = read_urls_from_file(file)
     e = fetch_and_deduplicate_content(e)
-    e = json_of_ip(e, list)
+    e = json_of_domain(e, file)
+    e = convert_json_to_srs(e)
+    return e
+
+
+def result_of_reject_domain(file):
+    e = read_urls_from_file(file)
+    e = fetch_and_deduplicate_content(e)
+    e = json_of_domain(e, file)
+    e = convert_json_to_srs(e)
+    return e
+
+
+def result_of_cn_domain(file):
+    e = read_urls_from_file(file)
+    e = fetch_and_deduplicate_cn_domain(e)
+    e = json_of_domain(e, file)
+    e = convert_json_to_srs(e)
+    return e
+
+
+def result_of_ip(file):
+    e = read_urls_from_file(file)
+    e = fetch_and_deduplicate_content(e)
+    e = json_of_ip(e, file)
     e = convert_json_to_srs(e)
     return e
 
@@ -167,8 +193,9 @@ def result_of_ip(list):
 def main():
     os.mkdir(output_dir)
     subprocess.run(['git', 'checkout', 'hidden'], check=True)
-    files = [blacklist, excludelist, blocklist]
-    result_of_gfw_domain(files, custom_excludelist)
+    result_of_gfw_domain(proxy_list, custom_exclude_list)
+    result_of_exclude_proxy_domain(exclude_proxy_list)
+    result_of_reject_domain(reject_list)
     result_of_cn_domain(geosite_cn)
     result_of_ip(geoip_cn)
 
