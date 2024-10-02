6ab9b3214982928fc3886da8e5da87e16ee8fa6f
HiRules <hitmp@outlook.com>
2024-10-02 15:17:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -63,7 +63,7 @@ def merge_json_files(folder_path):
     for key, values in rules_dict.items():
         merged_data['rules'].append({key: values})
     
-    filepath = os.path.join(test_dir, "at-cn.json")
+    filepath = os.path.join(test_dir, test_dir + ".json")
     with open(filepath, 'w') as f:
         f.write(json.dumps(merged_data, indent=4))
     return filepath
