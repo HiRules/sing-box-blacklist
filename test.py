1b0fdf76bf699b4316adef657c4a18af4ca5f024
HiRules <hitmp@outlook.com>
2024-10-02 10:35:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import git
 import requests
 import json
 import subprocess
@@ -18,8 +19,10 @@ repo_name = 'sing-box-blacklist'
 branch_name = 'hidden'
 file_name = 'geosite.db'
 
-print("1:" + os.getcwd())
-testpath = os.path.join(os.getcwd(), branch_name, file_name)
+repo = git.Repo(os.getcwd())
+repo.git.checkout('hidden')
+# print("1:" + os.getcwd())
+testpath = os.path.join(os.getcwd(), file_name)
 print("2:" + testpath)
 if os.path.isfile(testpath):
     size = os.path.getsize(testpath)
@@ -132,15 +135,15 @@ def result_of_ip(file):
 def main():
     os.mkdir(output_dir)
     subprocess.run(['git', 'checkout', 'hidden'], check=True)
-    categories = ["firebase@cn", "ubisoft@cn", "microsoft-pki@cn", "google-play@cn", "okaapps@cn", "thelinuxfoundation@cn", "amp@cn", "amd@cn", "kechuang@cn", "openjsfoundation@cn", "thetype@cn", "category-cryptocurrency@cn", "adobe@cn", "category-finance@cn", "muji@cn", "duolingo@cn", "asus@cn", "mapbox@cn", "bluearchive@cn", "aerogard@cn", "oreilly@cn", "test-ipv6@cn", "airwick@cn", "walmart@cn", "xbox@cn", "adidas@cn", "teamviewer@cn", "dell@cn", "pearson@cn", "starbucks@cn", "familymart@cn", "st@cn", "westerndigital@cn", "booking@cn", "reabble@cn", "fflogs@cn", "bluepoch-games@cn", "okx@cn", "mortein@cn", "acer@cn", "panasonic@cn", "movefree@cn", "epicgames@cn", "category-remote-control@cn", "gucci@cn", "hm@cn", "category-tech-media@cn", "calgoncarbon@cn", "youtube@cn", "hp@cn", "tvb@cn", "samsung@cn", "vanish@cn", "ifast@cn", "sslcom@cn", "tencent-dev@cn", "dettol@cn", "webex@cn", "vmware@cn", "bytedance@cn", "yahoo@cn", "shopee@cn", "finish@cn", "att@cn", "canon@cn", "jetbrains@cn", "swift@cn", "linkedin@cn", "verizon@cn", "clearasil@cn", "digicert@cn", "lysol@cn", "strepsils@cn", "kurogames@cn", "veet@cn", "meadjohnson@cn", "riot@cn", "bluepoch@cn", "woolite@cn", "category-social-media-!cn@cn", "msn@cn", "hketgroup@cn", "primevideo@cn", "nurofen@cn", "gigabyte@cn", "durex@cn", "razer@cn", "mcdonalds@cn", "farfetch@cn", "kindle@cn", "bestbuy@cn", "nvidia@cn", "miniso@cn", "bridgestone@cn", "qnap@cn", "intel@cn", "microsoft-dev@cn", "bmw@cn", "google-trust-services@cn", "cisco@cn", "apple-pki@cn", "category-media@cn", "apple-dev@cn", "ikea@cn", "tesla@cn", "gog@cn", "itunes@cn", "sectigo@cn", "category-enhance-gaming@cn", "globalsign@cn", "category-ntp-cn@cn", "icloud@cn", "mastercard@cn", "qualcomm@cn", "bilibili-game@cn", "bilibili@cn", "category-ntp@cn", "volvo@cn", "bing@cn", "beats@cn", "visa@cn", "akamai@cn", "category-dev@cn", "nintendo@cn", "paypal@cn", "mihoyo@cn", "mihoyo-cn@cn", "steam@cn", "cloudflare@cn", "cloudflare-cn@cn", "nike@cn", "category-entertainment-cn@cn", "aws@cn", "aws-cn@cn", "huawei-dev@cn", "huawei@cn", "category-cas@cn", "category-dev-cn@cn", "rb@cn", "ebay@cn", "amazon@cn", "azure@cn", "category-ecommerce@cn", "tencent-games@cn", "tencent@cn", "google@cn", "microsoft@cn", "category-games@cn", "category-entertainment@cn", "apple@cn", "geolocation-cn@cn", "category-companies@cn"]
+    # categories = ["firebase@cn", "ubisoft@cn", "microsoft-pki@cn", "google-play@cn", "okaapps@cn", "thelinuxfoundation@cn", "amp@cn", "amd@cn", "kechuang@cn", "openjsfoundation@cn", "thetype@cn", "category-cryptocurrency@cn", "adobe@cn", "category-finance@cn", "muji@cn", "duolingo@cn", "asus@cn", "mapbox@cn", "bluearchive@cn", "aerogard@cn", "oreilly@cn", "test-ipv6@cn", "airwick@cn", "walmart@cn", "xbox@cn", "adidas@cn", "teamviewer@cn", "dell@cn", "pearson@cn", "starbucks@cn", "familymart@cn", "st@cn", "westerndigital@cn", "booking@cn", "reabble@cn", "fflogs@cn", "bluepoch-games@cn", "okx@cn", "mortein@cn", "acer@cn", "panasonic@cn", "movefree@cn", "epicgames@cn", "category-remote-control@cn", "gucci@cn", "hm@cn", "category-tech-media@cn", "calgoncarbon@cn", "youtube@cn", "hp@cn", "tvb@cn", "samsung@cn", "vanish@cn", "ifast@cn", "sslcom@cn", "tencent-dev@cn", "dettol@cn", "webex@cn", "vmware@cn", "bytedance@cn", "yahoo@cn", "shopee@cn", "finish@cn", "att@cn", "canon@cn", "jetbrains@cn", "swift@cn", "linkedin@cn", "verizon@cn", "clearasil@cn", "digicert@cn", "lysol@cn", "strepsils@cn", "kurogames@cn", "veet@cn", "meadjohnson@cn", "riot@cn", "bluepoch@cn", "woolite@cn", "category-social-media-!cn@cn", "msn@cn", "hketgroup@cn", "primevideo@cn", "nurofen@cn", "gigabyte@cn", "durex@cn", "razer@cn", "mcdonalds@cn", "farfetch@cn", "kindle@cn", "bestbuy@cn", "nvidia@cn", "miniso@cn", "bridgestone@cn", "qnap@cn", "intel@cn", "microsoft-dev@cn", "bmw@cn", "google-trust-services@cn", "cisco@cn", "apple-pki@cn", "category-media@cn", "apple-dev@cn", "ikea@cn", "tesla@cn", "gog@cn", "itunes@cn", "sectigo@cn", "category-enhance-gaming@cn", "globalsign@cn", "category-ntp-cn@cn", "icloud@cn", "mastercard@cn", "qualcomm@cn", "bilibili-game@cn", "bilibili@cn", "category-ntp@cn", "volvo@cn", "bing@cn", "beats@cn", "visa@cn", "akamai@cn", "category-dev@cn", "nintendo@cn", "paypal@cn", "mihoyo@cn", "mihoyo-cn@cn", "steam@cn", "cloudflare@cn", "cloudflare-cn@cn", "nike@cn", "category-entertainment-cn@cn", "aws@cn", "aws-cn@cn", "huawei-dev@cn", "huawei@cn", "category-cas@cn", "category-dev-cn@cn", "rb@cn", "ebay@cn", "amazon@cn", "azure@cn", "category-ecommerce@cn", "tencent-games@cn", "tencent@cn", "google@cn", "microsoft@cn", "category-games@cn", "category-entertainment@cn", "apple@cn", "geolocation-cn@cn", "category-companies@cn"]
 
     
     # for category in categories:
     #     #filename = "geosite-" + category + ".json"
     #     os.system("sing-box geosite export " + category)
         
-    e = merge_json(meta_rules)
-    convert_json_to_srs(e)
+    # e = merge_json(meta_rules)
+    # convert_json_to_srs(e)
 
 
 if __name__ == "__main__":
