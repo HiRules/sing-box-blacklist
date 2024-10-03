e21d2692a5236fc51ffe516e7b398c92a796d5e9
HiRules <hitmp@outlook.com>
2024-10-03 11:02:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -151,6 +151,7 @@ def result_of_ip(file):
 def main():
     os.mkdir(output_dir)
     subprocess.run(['git', 'checkout', 'hidden'], check=True)
+    os.mkdir("folder_path")
     download_file(geosite, "folder_path")
     # download_file(geosite, testdir)
 
