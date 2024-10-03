d0b23cff2cdb6c495cb3d31ddc3771f6d44c536a
HiRules <hitmp@outlook.com>
2024-10-03 11:37:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ reject_list = 'reject_list.txt'
 geosite_cn = 'geosite_cn.txt'
 geoip_cn = 'geoip_cn.txt'
 custom_exclude_list = 'custom_exclude_list.txt'
+db_file = 'acer@cn.json'
 categories = ["tld-cn", "geolocation-cn", "apple-cn", "google-cn", "firebase@cn", "ubisoft@cn", "microsoft-pki@cn", "google-play@cn", "okaapps@cn", "thelinuxfoundation@cn", "amp@cn", "amd@cn", "kechuang@cn", "openjsfoundation@cn", "thetype@cn", "category-cryptocurrency@cn", "adobe@cn", "category-finance@cn", "muji@cn", "duolingo@cn", "asus@cn", "mapbox@cn", "bluearchive@cn", "aerogard@cn", "oreilly@cn", "test-ipv6@cn", "airwick@cn", "walmart@cn", "xbox@cn", "adidas@cn", "teamviewer@cn", "dell@cn", "pearson@cn", "starbucks@cn", "familymart@cn", "st@cn", "westerndigital@cn", "booking@cn", "reabble@cn", "fflogs@cn", "bluepoch-games@cn", "okx@cn", "mortein@cn", "acer@cn", "panasonic@cn", "movefree@cn", "epicgames@cn", "category-remote-control@cn", "gucci@cn", "hm@cn", "category-tech-media@cn", "calgoncarbon@cn", "youtube@cn", "hp@cn", "tvb@cn", "samsung@cn", "vanish@cn", "ifast@cn", "sslcom@cn", "tencent-dev@cn", "dettol@cn", "webex@cn", "vmware@cn", "bytedance@cn", "yahoo@cn", "shopee@cn", "finish@cn", "att@cn", "canon@cn", "jetbrains@cn", "swift@cn", "linkedin@cn", "verizon@cn", "clearasil@cn", "digicert@cn", "lysol@cn", "strepsils@cn", "kurogames@cn", "veet@cn", "meadjohnson@cn", "riot@cn", "bluepoch@cn", "woolite@cn", "category-social-media-!cn@cn", "msn@cn", "hketgroup@cn", "primevideo@cn", "nurofen@cn", "gigabyte@cn", "durex@cn", "razer@cn", "mcdonalds@cn", "farfetch@cn", "kindle@cn", "bestbuy@cn", "nvidia@cn", "miniso@cn", "bridgestone@cn", "qnap@cn", "intel@cn", "microsoft-dev@cn", "bmw@cn", "google-trust-services@cn", "cisco@cn", "apple-pki@cn", "category-media@cn", "apple-dev@cn", "ikea@cn", "tesla@cn", "gog@cn", "itunes@cn", "sectigo@cn", "category-enhance-gaming@cn", "globalsign@cn", "category-ntp-cn@cn", "icloud@cn", "mastercard@cn", "qualcomm@cn", "bilibili-game@cn", "bilibili@cn", "category-ntp@cn", "volvo@cn", "bing@cn", "beats@cn", "visa@cn", "akamai@cn", "category-dev@cn", "nintendo@cn", "paypal@cn", "mihoyo@cn", "mihoyo-cn@cn", "steam@cn", "cloudflare@cn", "cloudflare-cn@cn", "nike@cn", "category-entertainment-cn@cn", "aws@cn", "aws-cn@cn", "huawei-dev@cn", "huawei@cn", "category-cas@cn", "category-dev-cn@cn", "rb@cn", "ebay@cn", "amazon@cn", "azure@cn", "category-ecommerce@cn", "tencent-games@cn", "tencent@cn", "google@cn", "microsoft@cn", "category-games@cn", "category-entertainment@cn", "apple@cn", "geolocation-cn@cn", "category-companies@cn"]
 geosite = "https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/release/geosite.db"
 
@@ -150,21 +151,27 @@ def result_of_ip(file):
 
 def main():
     os.mkdir(output_dir)
-    ingpath = 'test'
-    os.mkdir(ingpath)
-    subprocess.run(['git', 'checkout', 'hidden'], check=True)
-    download_file(geosite, ingpath)
+    # ingpath = 'test'
+    # os.mkdir(ingpath)
+    subprocess.run(['git', 'checkout', 'dev001'], check=True)
+    # download_file(geosite, ingpath)
     # download_file(geosite, testdir)
 
-    # if os.path.isfile(db_file):
-    #     size = os.path.getsize(db_file)
-    #     print(size)
+    if os.path.isfile(db_file):
+        size = os.path.getsize(db_file)
+        print(size)
+    out = []
+    with open(db_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            out.append(line)
+        print(out)
         
     
-    get_category_file(categories)
+    # get_category_file(categories)
     
-    merged_json_data = merge_json_files(ingpath)
-    print(merged_json_data)
+    # merged_json_data = merge_json_files(ingpath)
+    # print(merged_json_data)
 
 
     # e = merge_json(meta_rules)
